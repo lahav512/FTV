9a0b078408e202a7a07eab55004386116e8cb384
lahav512 <lahavs512@gmail.com>
2020-03-23 23:10:57 +0200

    SetupMethods and setupBuiltinMethods have been successfully implemented into the DynamicModule object.

--- AppPackage/Experiments/methodAttribute.py
+++ AppPackage/Experiments/methodAttribute.py
@@ -1,104 +1,62 @@
+import inspect
 from functools import wraps, partial
 
 
-class FTVMethodVariables:
-    def __init__(self, func):
-        self.func = func
-        self.name = self.func.__name__
-        self.__triggers__ = []
-
-    def __call__(self, *args, **kwargs):
-        self.func(self, *args, **kwargs)
-
-    def addTrigger(self, trigger):
-        self.__triggers__.append(trigger)
-
-
-def FTVMethod(func=None):
-    if not func:
-        return partial(FTVMethod)
-
-    @FTVMethodVariables
-    def run_func():
-        return func()
-
-    @wraps(run_func)
-    def wrapper(*args, **kwargs):
-        print("-> " + func.__name__)
-        ans = func(*args, **kwargs)
-        print("<- " + func.__name__)
-        return ans
-    return wrapper
-
-
-class Feature1:
+class DynamicModuleParent(object):
+    __BUILTIN_METHODS = (
+        "__setattr__",
+        "__init__",
+        "addTrigger",
+        "addTriggers",
+        "_DynamicModuleParent__initMethodsVariables",
+        "_DynamicModuleParent__setMethodTriggers"
+    )
 
     def __setattr__(self, key, value):
-        if key in dir(self):
-            if callable(getattr(self, key)):
-                raise Exception(
-                    "Can't add the attribute \"{}\" to the object \"{}\", since it is already exists as a method.".format(
-                        key, self.__class__.__name__))
+        if key in dir(self) and callable(getattr(self, key)):
+            raise Exception(
+                "Can't add the attribute \"{}\" to the object \"{}\", since it is already exists as a method.".format(
+                    key, self.__class__.__name__))
         else:
             super().__setattr__(key, value)
 
-    def __getattribute__(self, item):
-        ans = object.__getattribute__(self, item)
-        if callable(ans):
-            print(item)
-            return ans
-        else:
-            return ans
-
     def __init__(self):
-        self.__setMethodTriggers([])
+        self._setupMethods()
+        self.addTriggers()
 
-        # self.action = "lahav"
+    def addTrigger(self, method, trigger):
+        method.__triggers__.append(trigger)
+
+    def addTriggers(self):
+        pass
+
+    def _setupMethods(self):
+        for func in inspect.getmembers(self, inspect.ismethod):
+            method_key = func[0]
+            if method_key not in getattr(self, "_DynamicModuleParent__BUILTIN_METHODS"):
+                self.__setMethodTriggers(method_key, [])
+
+    def __setMethodTriggers(self, method_key, triggers):
+        setattr(getattr(self.__class__, method_key), "__triggers__", triggers)
+
+
+class Feature1(DynamicModuleParent):
 
     def action(self):
         self.do()
 
-    def addTrigger(self, trigger):
-        self.__getMethodTriggers().append(trigger)
-
-    def __setMethodTriggers(self, triggers):
-        setattr(object.__getattribute__(self.__class__, "action"), "__triggers__", triggers)
-
-    def __getMethodTriggers(self) -> list:
-        return getattr(object.__getattribute__(self.__class__, "action"), "__triggers__")
-
     def do(self):
-        print("do")
+        print("do()")
 
-
-class Feature2:
-
-    def __init__(self):
-        self.a = 2
-
-    def __setattr__(self, key, value):
-        if key in dir(self):
-            if callable(getattr(self, key)):
-                raise Exception(
-                    "Can't add the attribute \"{}\" to the object \"{}\", since it is already exists as a method.".format(
-                        key, self.__class__.__name__))
-        else:
-            super().__setattr__(key, value)
-
-    @FTVMethodVariables
-    def action(self):
-        self.do()
-
-    def addTrigger(self, trigger):
-        self.action.__triggers__.append(trigger)
-
-    def do(self):
-        print("do")
+    def addTriggers(self):
+        self.addTrigger(self.action, "a")
+        self.addTrigger(self.action, "b")
+        self.addTrigger(self.action, "c")
+        self.addTrigger(self.do, "c")
 
 
 feature = Feature1()
-feature.addTrigger("a")
-feature.addTrigger("b")
-feature.addTrigger("c")
+
 print(feature.action.__triggers__)
+print(feature.do.__triggers__)
 feature.action()
--- FTV/Objects/SystemObjects/Trigger.py
+++ FTV/Objects/SystemObjects/Trigger.py
@@ -1,10 +1,9 @@
-from FTV.Objects.Variables.AbstractDynamicObject import DynamicObject
 
 
 class Trigger:
-    def __init__(self, dy_module_parent, dy_variable: DynamicObject, condition, action, thread=None):
+    def __init__(self, dy_module_parent, condition, action, thread=None):
         self.dy_module_parent = dy_module_parent
-        self.dy_variable = dy_variable
         self.condition = condition
         self.action = action
         self.thread = thread
+
