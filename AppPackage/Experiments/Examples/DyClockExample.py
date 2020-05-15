ff8c92df2952f2a2746c1d245037f71193b47175
lahav512 <lahavs512@gmail.com>
2020-05-16 02:36:08 +0300

    DyClockExample is stable except the recursion issue.

--- AppPackage/Experiments/Examples/DyClockExample.py
+++ AppPackage/Experiments/Examples/DyClockExample.py
@@ -13,54 +13,45 @@ from FTV.Objects.Variables.DynamicObjects import DyInt
 class VM(VariableManager):
 
     def setupVariables(self):
+        self.tenth_seconds_mode = 10
         self.seconds_mode = 60
         self.minutes_mode = 60
         self.hours_mode = 24
 
-        self.hours = DyInt(0)
-        self.minutes = DyInt(0)
+        self.tenth_seconds = DyInt(0)
         self.seconds = DyInt(0)
+        self.minutes = DyInt(0)
+        self.hours = DyInt(0)
 
     def setupTriggers(self):
-        self.addTrigger(self.seconds)\
-            .setCondition(DyInt.IsEqualTo, self.seconds_mode)\
-            .setAction(self.setSeconds, 0)
+        self.addTrigger(self.tenth_seconds)\
+            .setCondition(DyInt.IsGraterEqualTo, self.tenth_seconds_mode)\
+            .setAction(self.updateUnit, self.tenth_seconds, self.seconds, self.tenth_seconds_mode)
 
-        self.addTrigger(self.seconds) \
-            .setCondition(DyInt.IsEqualTo, 0) \
-            .setAction(self.addMinutes, 1)
+        self.addTrigger(self.seconds)\
+            .setCondition(DyInt.IsGraterEqualTo, self.seconds_mode)\
+            .setAction(self.updateUnit, self.seconds, self.minutes, self.seconds_mode)
 
         self.addTrigger(self.minutes)\
-            .setCondition(DyInt.IsEqualTo, self.minutes_mode)\
-            .setAction(self.setMinutes, 0)
-
-        self.addTrigger(self.minutes) \
-            .setCondition(DyInt.IsEqualTo, 0) \
-            .setAction(self.addHours, 1)
+            .setCondition(DyInt.IsGraterEqualTo, self.minutes_mode)\
+            .setAction(self.updateUnit, self.minutes, self.hours, self.minutes_mode)
 
         self.addTrigger(self.hours)\
-            .setCondition(DyInt.IsEqualTo, self.hours_mode)\
-            .setAction(self.setHours, 0)
+            .setCondition(DyInt.IsGraterEqualTo, self.hours_mode)\
+            .setAction(self.modUnit, self.hours, self.hours, self.hours_mode)
 
     @DyMethod()
-    def setHours(self, hours: int):
-        self.hours.set(hours)
+    def updateUnit(self, var_1: DyInt, var_2: DyInt, mod: int):
+        self.addUnit(var_2, var_1, mod)
+        self.modUnit(var_1, var_1, mod)
 
-    @DyMethod()
-    def setMinutes(self, minutes: int):
-        self.minutes.set(minutes)
+    # @DyMethod()
+    def modUnit(self, var: DyInt, val: DyInt, mod: int):
+        var.set(val.get() % mod)
 
-    @DyMethod()
-    def setSeconds(self, seconds: int):
-        self.seconds.set(seconds)
-
-    @DyMethod()
-    def addMinutes(self, minutes: int):
-        self.minutes += minutes
-
-    @DyMethod()
-    def addHours(self, hours: int):
-        self.hours += hours
+    # @DyMethod()
+    def addUnit(self, var: DyInt, val: DyInt, mod: int):
+        var += val.get() // mod
 
 
 class ClockFeature(DyModule):
@@ -79,13 +70,13 @@ class ClockFeature(DyModule):
         self.addTrigger(self.tick).setAction(self.tick)
 
     def getTimeStamp(self):
-        return f"{self.vm.hours}:{self.vm.minutes}:{self.vm.seconds}"
+        return f"{self.vm.hours}:{self.vm.minutes}:{self.vm.seconds}:{self.vm.tenth_seconds}"
 
     @DyMethod()
     def tick(self):
-        self.vm.seconds += 1
+        self.vm.tenth_seconds += 1
         Log.p(self.getTimeStamp())
-        time.sleep(1)
+        time.sleep(0.1)
 
 
 class EM(ExecutionManager):
