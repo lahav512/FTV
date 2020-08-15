d332f36e78e9409b76ba7075dd85a18c0e287e3a
lahav512 <lahavs512@gmail.com>
2020-08-15 17:06:42 +0300

    Create an app with various files is working. Please consider to provide a cleaner solution.

---
+++ AppPackage/Experiments/Examples/DyClockExample/ClockApp.py
@@ -0,0 +1,47 @@
+from AppPackage.Experiments.Log import Log
+from FTV.FrameWork.Apps import NIApp
+from FTV.Managers.ExecutionManager import ExecutionManager
+from FTV.Managers.FeatureManager import FeatureManager
+from FTV.Managers.VariableManager import VariableManager
+from FTV.Objects.SystemObjects.Executions import DyThread
+from FTV.Objects.Variables.DynamicObjects import DyInt
+
+
+class VM(VariableManager):
+    def setupVariables(self):
+        self.tenth_seconds = DyInt(0)
+        self.seconds = DyInt(0)
+        self.minutes = DyInt(0)
+        self.hours = DyInt(0)
+
+    def setupTriggers(self):
+        pass
+
+
+class EM(ExecutionManager):
+    def setupThreads(self):
+        self.MainUI = DyThread()
+
+
+class FM(FeatureManager):
+    def setupFeatures(self):
+        Log.p("-> setupFeatures")
+        from AppPackage.Experiments.Examples.DyClockExample.Features.IntegratedClock import IntegratedClock
+        from AppPackage.Experiments.Examples.DyClockExample.Features.VisualClock import VisualClock
+
+        self.addFeature(IntegratedClock)
+        self.addFeature(VisualClock)
+        Log.p("<- setupFeatures")
+
+
+class ClockApp(NIApp):
+
+    def setupSettings(self):
+        pass
+
+    def setupManagers(self):
+        Log.p("-> setupManagers")
+        self.setExecutionManager(EM)
+        self.setFeatureManager(FM)
+        self.setVariableManager(VM)
+        Log.p("<- setupManagers")
