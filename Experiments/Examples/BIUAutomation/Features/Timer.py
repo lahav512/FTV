3064d289cde74d279bd231c50cf65b1c786a6283
Lahav <lahavs512@gmail.com>
2022-03-31 23:28:02 +0300

    Timer feature has been added to the BIUAutomation example project. It was relatively easy, but the timer should be a part of the FTV toolbox to allow fast development.
    Signed-off-by: Lahav <lahavs512@gmail.com>

---
+++ Experiments/Examples/BIUAutomation/Features/Timer.py
@@ -0,0 +1,56 @@
+import time
+
+from Bots.GenerateMagicMethods.result.MagicMethodsInterfaces import DyObject
+from Experiments.Examples.DyClockExample.ClockApp import ClockApp
+from FTV.Managers.Log import Log
+from FTV.FrameWork.Features import NIFeature
+from FTV.Managers.VariableManager import VariableManager
+from FTV.Objects.SystemObjects.TriggerObjects import Condition
+from FTV.Objects.Variables.DynamicMethods import DyMethod, DyBuiltinMethod
+from FTV.Objects.Variables.DynamicObjects import DyInt
+
+
+class VM(VariableManager):
+    def setupVariables(self):
+        self.isStopped = False
+        self.seconds = DyInt(-1, builtin=False)
+        self.period = 10  # seconds
+
+    def setupTriggers(self):
+        pass
+
+    class PeriodCompleted(Condition):
+        @staticmethod
+        def __condition__(old_val, new_val, *args, **kwargs):
+            return new_val % args[0] == 0
+
+
+class Timer(NIFeature):
+    def setupSettings(self):
+        pass
+
+    def setupManagers(self):
+        self.setVariableManager(VM)
+
+    def setupTriggers(self):
+        from Experiments.Examples.BIUAutomation.BIUApp import BIUApp
+
+        self.overrideTriggers(BIUApp.vm.START).setAction(self.startClock)
+        self.addTrigger(self.vm.seconds).setCondition(self.vm.PeriodCompleted, self.vm.period).setAction(BIUApp.vm.onStartCollection)
+
+    def stop(self):
+        self.vm.isStopped = True
+
+    @DyMethod()
+    def updateDyObject(self, obj: DyObject, new_obj: DyObject):
+        obj.set(new_obj.get())
+
+    @DyMethod()
+    def startClock(self):
+        while not self.vm.isStopped:
+            self.tick()
+
+    @DyBuiltinMethod()
+    def tick(self):
+        self.vm.seconds += 1
+        time.sleep(1)
